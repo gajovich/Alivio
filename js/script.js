59220af9c659363b09b08db549bca17ec94b57a7
gajovich <75524971+gajovich@users.noreply.github.com>
2021-08-05 11:15:20 +0300

    first site

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.icon-menu').addEventListener('click', function () {
+   document.querySelector('.icon-menu').classList.toggle('active');
+   document.querySelector('.menu__list').classList.toggle('active');
+})
+
+function ibg() {
+   let ibg = document.querySelectorAll(".ibg");
+   for (var i = 0; i < ibg.length; i++) {
+      if (ibg[i].querySelector('img')) {
+         ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+      }
+   }
+}
+
+ibg();
